2a36d1ec9f776100d2c973089de338a5fe095c99
SHAIK RIZWANA <155900035+SHAIKRIZWANA16@users.noreply.github.com>
2025-12-17 11:07:33 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root', // your MySQL user
-  password: 'root@123', // your MySQL password
+  password: 'your_pswd', // your MySQL password
   database: 'tutor_app' // database you created
 });
 exports.db = db;
